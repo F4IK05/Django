42979071ca5d1bb66be03ced0398f358f50c860e
Faik Hasanov <faik.hasanov05@gmail.com>
2026-03-17 16:04:41 +0400

    Django 03

---
+++ Django 03/task.py
@@ -0,0 +1,9 @@
+def fibonacci_generator(n):
+    a, b = 0, 1
+    for _ in range(n):
+        yield a
+        a, b = b, a + b
+
+fib_gen = fibonacci_generator(10)
+for _ in range(10):
+    print(next(fib_gen))
